dc05b7360e2a0d8e5731168b1a3abacd1a275983
BaHuy15 <huyn0995789648@gmail.com>
2023-07-05 10:48:25 +0800

    update command to run file

--- command/train.sh
+++ command/train.sh
@@ -1,19 +1,4 @@
-# python3 segment/train.py \
-# --weights=/home/charleschinh/TOMO/yolov7_mask/yolov7-seg.pt \
-# --data=/home/charleschinh/TOMO/TrainData/coco.yaml \
-# --hyp=/home/charleschinh/TOMO/yolov7_mask/seg/data/hyps/custom_hyps.yaml \
-# --epochs=300 \
-# --device=2 \
-# --project=/home/charleschinh/TOMO/TrayBOM/train-seg
-
-# python3 segment/train.py \
-# --weights=/home/charleschinh/TOMO/yolov7_mask/yolov7-seg.pt \
-# --data=/home/tonyhuy/TOMO/Instance_segmentation/yolov7_mask/seg/data/coco.yaml \
-# --hyp=/home/tonyhuy/TOMO/Instance_segmentation/yolov7_mask/seg/data/hyps/hyp.scratch-med.yaml\
-# --epochs=300 \
-# --device=3 \
-# --project=/home/tonyhuy/TOMO/Instance_segmentation/yolov7_mask
-
+# Run training
 python3 segment/train.py \
 --weights=/home/tonyhuy/TOMO/Instance_segmentation/yolov7_mask/seg/yolov5s-seg.pt \
 --data=/home/tonyhuy/bottle_classification/seg/data/coco.yaml \
